1022005ddcc49c5c92944da6ac7f72523378c618
leicaflorian <florian.leica@gmail.com>
2022-12-09 18:43:52 +0100

    Fixes merge command

--- release.hook.afterRelease.js
+++ release.hook.afterRelease.js
@@ -3,12 +3,13 @@ const shell = require('shelljs')
 
 const mergeToBranches = ['main']
 const workingBranch = process.argv[2]
+const commitMessage = process.argv[3]
 
 mergeToBranches.forEach(branch => {
   logs.sectionStart(`Merging release branch to "${branch}"`)
   
   shell.exec(`git checkout ${branch}`, { silent: true })
-  shell.exec(`git merge ${workingBranch}`, { silent: false })
+  shell.exec(`git merge ${workingBranch} -m ${commitMessage}`, { silent: false })
   shell.exec(`git push origin ${branch}`, { silent: true })
   
   // return to original branch
