4045238453af1e12a0948cdd2c0d071d4fef24f9
leicaflorian <florian.leica@gmail.com>
2022-12-09 17:34:02 +0100

    Add version file to git

--- bin/utilities/exportVersion.js
+++ bin/utilities/exportVersion.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const path = require('path')
+const shell = require('shelljs')
 const logs = require('./logs')
 
 module.exports.exportVersion = function (newVersion, filePath) {
   logs.sectionStart(`Storing version in "${filePath}"`)
   
   fs.writeFileSync(path.resolve(filePath), newVersion)
+  
+  shell.exec(`git add ${filePath}`)
   
   logs.sectionEnd('Version stored successfully!')
 }
